a19f301cbcd9b4123011396abf3dde4bd6668a87
Marco Pontiz <marcopontiz@gmail.com>
2023-04-22 17:14:53 -0300

    Tratando de implementar mongodb Atlas

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const { mongodb } = require('./keys');
 
-mongoose.connect(mongodb.URI, {useNewUrlParser: true})
+mongoose.connect(mongodb.URI_ATLAS, {useNewUrlParser: true})
     .then(db => console.log('La base de datos esta conectada'))
     .catch(err => console.error(err));
--- src/keys.js
+++ src/keys.js
@@ -1,5 +1,6 @@
 module.exports = {
     mongodb:{
-        URI:'mongodb://localhost:27017/register'
+        URI:'mongodb://localhost:27017/register',
+        URI_ATLAS: 'mongodb+srv://MarcoPontiz:OZaym5VSleg1PJRs@project-001.ndvaexq.mongodb.net/test'
     }
 };
