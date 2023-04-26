827df8a71417dc25f65f4885a38542dbd34d0399
Marco Pontiz <marcopontiz@gmail.com>
2023-04-26 17:41:59 -0300

    Censura key de la base de datos mongoDB ATLAS y localhost

--- src/keys.js
+++ src/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
     mongodb:{
-        URI:'mongodb://localhost:27017/register',
-        URI_ATLAS: 'mongodb+srv://MarcoPontiz:OZaym5VSleg1PJRs@project-001.ijlkqkx.mongodb.net/test' //me guarda el registro de usuario pero no muestra los productos
+        URI:'xxxxxx',
+        URI_ATLAS: 'xxxxxx' //me guarda el registro de usuario pero no muestra los productos
     }
 };
